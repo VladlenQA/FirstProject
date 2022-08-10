92b27e6fec2288042b0517aac016febed0ba3c88
vladlenkrivoy <vladikkryvoi@gmail.com>
2022-08-10 21:59:53 +0300

    lesson 3 snegovik

---
+++ src/com/company/Lesson3/China.java
@@ -0,0 +1,21 @@
+package com.company.Lesson3;
+
+public class China {
+    public static void main(String[] args) {
+        int livoin = 13;
+        int liluchnik = 24;
+        int livershnik = 26;
+        int liammount = 860;
+        int liatack = 860 * (13 + 24 + 46);
+
+        int minvoin = 9;
+        int minluchnik = 35;
+        int minvershnik = 12;
+        double minammount = liatack * 1.5;
+        int minatack = 1290 * (9 + 35 + 12);
+
+        System.out.println("Загальний показник атаки династії Лі = " + liatack);
+        System.out.println("Загальний показник атаки династії Мінь = " + (int) minatack);
+
+    }
+}
