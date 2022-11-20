4e1fe83b15150125dbf86d2f18d3b636b480438e
vladlenkrivoy <vladikkryvoi@gmail.com>
2022-11-20 14:45:00 +0200

    lesson 4 homework

--- src/com/company/lesson4/homework/Person.java
+++ src/com/company/lesson4/homework/Person.java
@@ -1,4 +1,13 @@
 package com.company.lesson4.homework;
 
 public class Person {
+    public static void main(String[] args) {
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "2936729462568"));
+    }
+
+    public static String personInfo(String firstName, String lastName, String city, String phone){
+        return "Зателефонувати громадянину " + firstName + " " + lastName + " із міста " + city + " можливо за номером " + phone;
+    }
 }
