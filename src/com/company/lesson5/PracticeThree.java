21e0b60f76c3fced3a511c252a5918b19e9264b6
vladlenkrivoy <vladikkryvoi@gmail.com>
2022-12-06 22:45:30 +0200

    lesson 4 changes

--- src/com/company/lesson5/PracticeThree.java
+++ src/com/company/lesson5/PracticeThree.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class PracticeThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter value: ");
+        boolean hasNextInt = scanner.hasNextInt();
         int a = scanner.nextInt();
+
+        String result = hasNextInt ? "Value integer" : "Other type";
+        System.out.println("Check is int: " + result);
     }
 }
